5d3a5b4f85594f1eedb59688e81d9acb99287c78
Luis Flores <me@luisflores.mx>
2014-02-02 16:26:33 +0000

    Tidy folders

---
+++ template.cpp
@@ -0,0 +1,50 @@
+#include <cctype>
+#include <string>
+#include <cstring>
+#include <climits>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <iostream>
+#include <sstream>
+#include <fstream>
+#include <iomanip>
+#include <vector>
+#include <list>
+#include <set>
+#include <map>
+#include <stack>
+#include <queue>
+#include <algorithm>
+#include <utility>
+#include <bitset>
+#include <limits> 
+
+using namespace std;
+
+#define PB(x) 			push_back(x)
+#define MP(x,y)			make_pair(x,y)
+#define ALL(x)			x.begin(), x.end()
+#define RALL(x)			x.rbegin(), x.rend()
+#define READ(x) 		freopen(x, "r", stdin)
+#define WRITE(x) 		freopen(x, "w", stdout)
+#define SORT(x)     sort(ALL(x))
+#define DREP(x)     sort(ALL(x)); x.erase(unique(ALL(x)),x.end())
+#define CLEAR(c) 		memset(c, 0, sizeof(c))
+#define P(x)				">>> " << #x << " : " << x << endl
+#define C(x) 				cout << P(x)
+#define CC(x) 			clog << P(x)         
+
+struct _ { ios_base::Init i; _() { cin.sync_with_stdio(0); cin.tie(0); } } _;
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+
+const int INF 	 = 0x3f3f3f3f;
+const double EPS = 1e-7;
+const double PI  = atan(1)*4;
+
+int main() {
+
+}
