0f9b7b7317253db8883990e987465246dca2cd90
Luis Flores <me@luisflores.mx>
2014-02-02 18:23:39 +0000

    Remove tabs

--- Mathematics/gcd.cpp
+++ Mathematics/gcd.cpp
@@ -6,8 +6,8 @@ unsigned gcd (unsigned n1, unsigned n2) {
 }
 
 int main() {
-	assert( gcd(2, 1) == 1 );
-	assert( gcd(2001, 25001) == 23 );
-	assert( gcd(96, 42) == gcd(42,96) );
-	assert( gcd(2*2*3*3*7, 2*2*2*3*5) ==  2*2*3 );
+  assert( gcd(2, 1) == 1 );
+  assert( gcd(2001, 25001) == 23 );
+  assert( gcd(96, 42) == gcd(42,96) );
+  assert( gcd(2*2*3*3*7, 2*2*2*3*5) ==  2*2*3 );
 }
