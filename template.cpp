8bf7d97e7913dda88ad2e8e74d21a221379044c5
Luis Flores <me@luisflores.mx>
2014-08-20 22:45:30 -0500

    Add primes reference

--- template.cpp
+++ template.cpp
@@ -25,15 +25,15 @@ using namespace std;
 #define PB(x)       push_back(x)
 #define MP(x,y)     make_pair(x,y)
 #define ALL(x)      x.begin(), x.end()
-#define RALL(x)		 x.rbegin(), x.rend()
-#define READ(x) 		freopen(x, "r", stdin)
-#define WRITE(x) 	 freopen(x, "w", stdout)
+#define RALL(x)     x.rbegin(), x.rend()
+#define READ(x)     freopen(x, "r", stdin)
+#define WRITE(x)    freopen(x, "w", stdout)
 #define SORT(x)     sort(ALL(x))
 #define DREP(x)     sort(ALL(x)); x.erase(unique(ALL(x)),x.end())
-#define CLEAR(c) 	 memset(c, 0, sizeof(c))
-#define P(x)				">>> " << #x << " : " << x << endl
-#define C(x) 			 cout << P(x)
-#define CC(x) 			clog << P(x)
+#define CLEAR(c)    memset(c, 0, sizeof(c))
+#define P(x)        ">>> " << #x << " : " << x << endl
+#define C(x)        cout << P(x)
+#define CC(x)       clog << P(x)
 
 struct _ { ios_base::Init i; _() { cin.sync_with_stdio(0); cin.tie(0); } } _;
 
@@ -43,9 +43,9 @@ typedef vector<ll> vl;
 typedef pair<int, int> ii;
 typedef vector<ii> vii;
 
-const int INF 	 = 0x3f3f3f3f;
-const double EPS = 1e-7;
-const double PI  = atan(1)*4;
+#define EPS 1e-14
+#define INF 0x3f3f3f3f;
+#define PI atan(1)*4;
 
 int main() {
 
