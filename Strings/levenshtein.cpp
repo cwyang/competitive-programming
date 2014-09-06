1399464b878f12f0f74d923b3c1a212b025eb34e
Luis Flores <me@luisflores.mx>
2014-09-05 20:17:35 -0500

    Tidy code

--- Strings/levenshtein.cpp
+++ Strings/levenshtein.cpp
@@ -10,7 +10,7 @@ template<class T> unsigned int levenshteinDistance(const T &s1, const T & s2) {
     col[0] = i+1;
     for (unsigned int j = 0; j < len2; j++) {
       col[j+1] = min(min(prevCol[1 + j] + 1, col[j] + 1),
-                     prevCol[j] + (s1[i]==s2[j] ? 0 : 1));
+                     prevCol[j] + (s1[i] == s2[j] ? 0 : 1));
     }
     col.swap(prevCol);
   }
