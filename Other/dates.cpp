f0ba97dfbae7ae1161a1f35f3ecbcad61fbe1e0f
Luis Flores <me@luisflores.mx>
2014-09-05 20:20:32 -0500

    Add Julian day formula with time

--- Other/dates.cpp
+++ Other/dates.cpp
@@ -1,5 +1,5 @@
-int intToDay(int jd) { 
-  return jd % 7; 
+int intToDay(int jd) {
+  return jd % 7;
 }
 
 int dateToInt(int y, int m, int d) {
@@ -7,6 +7,16 @@ int dateToInt(int y, int m, int d) {
         367 * (m - 2 - (m - 14) / 12 * 12) / 12 -
         3 * ((y + 4900 + (m - 14) / 12) / 100) / 4 +
         d - 32075;
+}
+
+double dateToDouble(int y, int m, int d, int hour, int minute, int second) {
+    int JDN = 1461 * (y + 4800 + (m - 14) / 12) / 4 +
+        367 * (m - 2 - (m - 14) / 12 * 12) / 12 -
+        3 * ((y + 4900 + (m - 14) / 12) / 100) / 4 +
+        d - 32075;
+    return (double)JDN + (((double)hour - 12.0) / 24.0) +
+           ((double)minute / 1440.0) +
+           ((double)second / 86400.0);
 }
 
 void intToDate(int jd, int &y, int &m, int &d) {
